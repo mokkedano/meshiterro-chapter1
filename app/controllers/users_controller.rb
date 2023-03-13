627821e20ad184c8bac62b2074daa8eff340cdf4
mokkedano <126681793+mokkedano@users.noreply.github.com>
2023-03-13 09:02:05 +0000

    [Finish] 4-17

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,15 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(user.id)
+    redirect_to user_path
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 
 end
